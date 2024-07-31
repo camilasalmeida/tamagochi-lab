f6bb465a4303eca554abfa027a801008f4685457
Camila Alm <camila.salmeidatech@hotmail.com>
2024-07-30 22:29:51 -0400

    step 2 pass

--- js/app.js
+++ js/app.js
@@ -12,13 +12,28 @@ let state = {
 
 let timer, gameOver; //this will assign the `timer` AND `gameOver` a value of undefined.
 
-
-
-
 /*------------------------ Cached Element References ------------------------*/
+const boredomStatEl = document.querySelector('#boredom-stat')
+const hungerStatEl = document.querySelector('#hunger-state')
+const sleepinessStatEl = document.querySelector('#sleepiness-stat')
 
+const playBtnEl = document.querySelector("#play")
+const feedBtnEl = document.querySelector("#feed")
+const sleepBtnEl = document.querySelector("#play")
 
+const gameMessageEl = document.querySelector('#message')
+const resetBtnEl = document.querySelector('#restart')
 
+console.log({
+    boredomStatEl,
+    hungerStatEl,
+    sleepinessStatEl,
+    playBtnEl,
+    feedBtnEl,
+    sleepBtnEl,
+    gameMessageEl,
+    resetBtnEl
+})
 /*-------------------------------- Functions --------------------------------*/
 
 
